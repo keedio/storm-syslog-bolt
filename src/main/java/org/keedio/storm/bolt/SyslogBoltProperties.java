1b882de8b1c159dc13ac70609a742b0d08ade99d
Marcelo <mvalle@keedio.com>
2017-02-10 09:27:18 +0100

    Properties move to independent class.

---
+++ src/main/java/org/keedio/storm/bolt/SyslogBoltProperties.java
@@ -0,0 +1,10 @@
+package org.keedio.storm.bolt;
+
+public class SyslogBoltProperties {
+	static final String SYSLOG_BOLT_HOST = "syslog.bolt.host";
+    static final String SYSLOG_BOLT_PORT = "syslog.bolt.port";
+	static final String SYSLOG_BOLT_ENRICHED = "syslog.bolt.enriched";
+	static final String SYSLOG_BOLT_PROTOCOL = "syslog.bolt.protocol";
+	static final String SYSLOG_BOLT_CSV_FILE_PATH = "syslog.bolt.csvFilePath";
+	static final String SYSLOG_BOLT_HDFS_ROOT = "syslog.bolt.hdfsRoot";
+}
